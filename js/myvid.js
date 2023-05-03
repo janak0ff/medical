e9146d847d250cdc94d83df42694b59a6d09a30c
Janak kr shrestha <janakshresthapro@gmail.com>
2023-05-03 22:17:51 +0545

    first commit

---
+++ js/myvid.js
@@ -0,0 +1,9 @@
+
+// var video_idname = "1xYLyOb8xxh1QG/Acne";
+// var vdorul = "https://" + video_idname + ".mp4";
+// document.getElementById("acnevdo").setAttribute("href", vdorul);
+// // ------------------------------
+
+
+
+
